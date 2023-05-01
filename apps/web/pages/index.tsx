15a56a9393dc6010d2f7f122b746eed79c74fad4
Robin Heidenis <robin.heidenis@gmail.com>
2023-05-01 14:12:53 +0200

    feat: 🎸 set up monorepo using turborepo

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,11 @@
+import { sub } from "@isildur/api";
 import { Button } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      {sub(1, 2)}
       <Button />
     </div>
   );
